f53e86b877a6eeda0bd1506cf69c129fee82b1c1
Vivek Kumar Yadav <vivekyadav07082001@gmail.com>
2025-02-08 17:18:14 +0530

    Create Database Connection DatabaseConnection

---
+++ Database Connection DatabaseConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/UserDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "yourpassword";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
